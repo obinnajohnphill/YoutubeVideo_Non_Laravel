fb16f24ad7fb02a4ca254e688f531f011ce48c49
Obinna Johnphill <obinna.johnphill@enta.net>
2018-11-14 19:00:45 +0000

    strated vue.js view templating

--- public/views/home.php
+++ public/views/home.php
@@ -1,3 +1,22 @@
+
+<html>
+<head>
+
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
+
+
+</head>
+
+<body>
+
+
+<form>
+<p>Message is: {{ message }}</p>
+<input v-model="message" placeholder="search video">
+<input type="checkbox" id="checkbox" v-model="checked">
+<label for="checkbox">{{ checked }}</label>
+</form>
+
 <?php
 
 /**
@@ -9,15 +28,20 @@
 
 include dirname(__FILE__).'/../../vendor/autoload.php';
 
-use Obinna\YoutubeVideosModel;
-use Obinna\Controllers\YoutubeVideosController;
+//use Obinna\YoutubeVideosModel;
+//use Obinna\Controllers\YoutubeVideosController;
 
 
-$user = new YoutubeVideosModel();
-$page = new YoutubeVideosController();
+//$user = new YoutubeVideosModel();
+//$page = new YoutubeVideosController();
 
-$helloUser = $user->sayhello();
-$hellopage = $page->another();
+//$helloUser = $user->sayhello();
+//$hellopage = $page->another();
 
-echo $helloUser;
-echo $hellopage;
+
+?>
+
+
+</body>
+</html>
+
