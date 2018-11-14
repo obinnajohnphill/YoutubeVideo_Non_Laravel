7a027995ad1da624bc142c8d75435fec31093f26
Obinna Johnphill <obinna.johnphill@enta.net>
2018-11-14 17:00:09 +0000

    added autolaod files into project

---
+++ autoload/src/Controllers/YoutubeVideosController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Obinna\Page;
+
+class YoutubeVideosController {
+    function another()
+    {
+        echo "hello again,this text comes from another class in src/YoutubeVideosController/YoutubeVideosController.phpideosController.php";
+    }
+}
---
+++ autoload/src/YoutubeVideosModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Obinna;
+
+class User {
+    public function sayhello()
+    {
+        echo "hello world, this text comes from a user class in src/User.php<br><br>";
+    }
+}
