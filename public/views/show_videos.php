a69dfd839b429fa294b87fb267463f6a71571d80
Obinna Johnphill <obinna.johnphill@enta.net>
2018-11-16 11:02:37 +0000

    refactored show-videos view

--- public/views/show_videos.php
+++ public/views/show_videos.php
@@ -9,61 +9,10 @@
 
         body {
             font-family: Arial;
-            width: 900px;
-            padding: 10px;
-        }
-        .search-form-container {
-            background: #F0F0F0;
-            border: #e0dfdf 1px solid;
-            padding: 20px;
-            border-radius: 2px;
-        }
-        .input-row {
-            margin-bottom: 20px;
-        }
-        .input-field {
             width: 100%;
-            border-radius: 2px;
             padding: 10px;
-            border: #e0dfdf 1px solid;
-        }
-        .btn-submit {
-            padding: 10px 20px;
-            background: #333;
-            border: #1d1d1d 1px solid;
-            color: #f0f0f0;
-            font-size: 0.9em;
-            width: 100px;
-            border-radius: 2px;
-            cursor:pointer;
-        }
-        .videos-data-container {
-            background: #F0F0F0;
-            border: #e0dfdf 1px solid;
-            padding: 20px;
-            border-radius: 2px;
         }
 
-        .response {
-            padding: 10px;
-            margin-top: 10px;
-            border-radius: 2px;
-        }
-
-        .error {
-            background: #fdcdcd;
-            border: #ecc0c1 1px solid;
-        }
-
-        .success {
-            background: #c5f3c3;
-            border: #bbe6ba 1px solid;
-        }
-        .result-heading {
-            margin: 20px 0px;
-            padding: 20px 10px 5px 0px;
-            border-bottom: #e0dfdf 1px solid;
-        }
         iframe {
             border: 0px;
         }
