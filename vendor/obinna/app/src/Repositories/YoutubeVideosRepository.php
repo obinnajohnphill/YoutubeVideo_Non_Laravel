d159e2fd90da24cebcf3b82a113d8d6fa50f9bda
Obinna Johnphill <obinna.johnphill@googlemail.com>
2018-11-24 00:00:43 +0000

    refactored ORM

--- vendor/obinna/app/src/Repositories/YoutubeVideosRepository.php
+++ vendor/obinna/app/src/Repositories/YoutubeVideosRepository.php
@@ -17,20 +17,30 @@ class YoutubeVideosRepository extends YoutubeVideosModel
 
     public $data;
     public $duplicate;
+    public $conn;
+
+    public function __construct()
+    {
+        $this->conn = new PDO("mysql:host=$this->host ;dbname=$this->db", $this->user, $this->pass);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
 
     public function all(){
-
-        $statement  = $this->conn->prepare("SELECT * FROM  videos");
-        $statement ->execute();
-        while ( $row = $statement->fetch(PDO::FETCH_ASSOC))
-        {
-            $videoId[] = $row['video_id'];
-            $title[] = $row['title'];
-            $this->data = array("videoId"=>$videoId,"title"=>$title);
+        try{
+            $statement  = $this->conn->prepare("SELECT * FROM  videos");
+            $statement ->execute();
+            while ( $row = $statement->fetch(PDO::FETCH_ASSOC))
+            {
+                $videoId[] = $row['video_id'];
+                $title[] = $row['title'];
+                $this->data = array("videoId"=>$videoId,"title"=>$title);
+            }
+            return  $this->data;
         }
-        $this->close(); ## Close pdo-mysql connection
-
-        return  $this->data;
+        catch(PDOException $e)
+        {
+        echo "Select all failed: " . $e->getMessage();
+        }
     }
 
     public function saveAll($video_id,$title)
@@ -39,12 +49,12 @@ class YoutubeVideosRepository extends YoutubeVideosModel
         try {
             $statement = $this->conn->prepare("INSERT INTO videos (video_id, title) VALUES ('$video_id','$title')");
             $statement->execute();
+            $statement = null;
         }
         catch(PDOException $e)
         {
             echo "Insert failed: " . $e->getMessage();
         }
-        $this->close(); ## Close mysqli connection
         session_start();
         $payload ['msg'] = "Your video has been saved";
         $redirect = "../saved_videos?".http_build_query($payload);;
@@ -53,25 +63,38 @@ class YoutubeVideosRepository extends YoutubeVideosModel
 
 
     public function checkDuplicate($video_id,$title){
-        $query  = "SELECT * FROM  videos WHERE video_id = '$video_id'";
-        $result = mysqli_query($this->connect(), $query);
-        if ($result->num_rows > 0) {
-            $this->close();
-            session_start();
-            $data[] = $title;
-            $payload ['msg'] = "Duplicate video exists in database";
-            $payload ['title'] = $data;
-            $redirect = "../?" . http_build_query($payload);;
-            header("Location: $redirect");
-        }else{
-            $this->duplicate = true;
+        try {
+            $statement = $this->conn->prepare("SELECT * FROM  videos WHERE video_id = '$video_id'");
+            $statement->execute();
+            $number_of_rows = $statement->fetchColumn();
+            if ($number_of_rows > 0) {
+                session_start();
+                $data[] = $title;
+                $payload ['msg'] = "Duplicate video exists in database";
+                $payload ['title'] = $data;
+                $redirect = "../?" . http_build_query($payload);;
+                header("Location: $redirect");
+            } else {
+                $this->duplicate = true;
+            }
+            return $this->duplicate;
         }
-        return $this->duplicate;
+        catch(PDOException $e)
+        {
+            echo "Check num-rows failed: " . $e->getMessage();
+        }
     }
 
+
     public function delete($video_id){
-        $query  = "DELETE FROM videos WHERE video_id = '$video_id'";
-        mysqli_query($this->connect(), $query);
+        try{
+            $statement = $this->conn->prepare("DELETE FROM videos WHERE video_id = '$video_id'");
+            $statement->execute();
+        }
+        catch(PDOException $e)
+        {
+        echo "Delete failed: " . $e->getMessage();
+        }
         session_start();
         $payload ['delete-msg'] = "Videos deleted from database";
         $redirect = "../saved_videos?" . http_build_query($payload);;
--- vendor/obinna/app/src/YoutubeVideosModel.php
+++ vendor/obinna/app/src/YoutubeVideosModel.php
@@ -1,44 +1,19 @@
 <?php
 
+/**
+ * Created by PhpStorm.
+ * User: obinnajohnphill
+ * Date: 14/11/18
+ * Time: 18:22
+ */
 namespace Obinna;
-use PDO;
-use PDOException;
+
 
 class YoutubeVideosModel {
 
-    public $host = "192.168.10.10";
-    public $user = "homestead";
-    public $pass = "secret";
-    public $db = "youtube_video";
-    public $conn;
-
-    function __construct()
-    {
-        try {
-            $this->conn = new PDO("mysql:host=$this->host ;dbname=$this->db", $this->user, $this->pass);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
-        }
-        catch(PDOException $e)
-        {
-            echo "Connection failed: " . $e->getMessage();
-        }
-
-        return $this->conn;
-
-    }
-
-    function connect() {
-        $con = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
-        if (!$con) {
-            die('Could not connect to database!');
-        }else {
-            $this->conn = $con;
-        }
-        return $this->conn;
-    }
-
-    function close() {
-       return  $this->conn = null;
-    }
+    protected $host = "192.168.10.10";
+    protected $user = "homestead";
+    protected $pass = "secret";
+    protected $db = "youtube_video";
+    protected$conn;
 }
